98b5de14822d96f1cb31909f51a6bf9a25d13591
Tariq Ismail <tariqismail@gmail.com>
2020-11-03 14:40:14 +0400

    fixed rating validation to only accept integer between 0 and 5

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,9 @@ class Review < ApplicationRecord
 
   validates :restaurant, presence: true
   validates :content, presence: true
-  validates :rating, presence: true, inclusion: { in: (0..5) }
+  validates :rating, presence: true, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 0,
+    less_than_or_equal_to: 5
+  }
 end
