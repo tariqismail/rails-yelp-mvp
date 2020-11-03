fded0a7b77993dc19290445f0966e682aecd3aba
Tariq Ismail <tariqismail@gmail.com>
2020-11-03 16:45:17 +0400

    added reviews to restaurants show view

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,7 +8,7 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
-    if @restaurant.save
+    if @review.save
       redirect_to @restaurant
     else
       render :new
