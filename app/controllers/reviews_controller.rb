5f196ceca88c8896430961f002fbf387cbacdc15
Tariq Ismail <tariqismail@gmail.com>
2020-11-03 16:27:38 +0400

    added reviews new and create actions and views

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,27 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: %i[new create]
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    if @restaurant.save
+      redirect_to @restaurant
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
 end
